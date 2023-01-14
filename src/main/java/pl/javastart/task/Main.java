b6e7e820ac9122e60940da6c224d6b421bd0113e
Monika Dabrowska <monikad71@onet.pl>
2023-01-14 17:06:28 +0100

    Solution 14.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,34 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final int EXIT = 0;
+    private static final int CREATE_AND_ADD_VEHICLE = 1;
+    private static final int NEXT_VEHICLE = 2;
+    private static Scanner scan = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int userInput;
+        do {
+            System.out.println("Wybierz opcję:");
+            printOptions();
+            userInput = scan.nextInt();
+            scan.nextLine();
+
+            switch (userInput) {
+                case CREATE_AND_ADD_VEHICLE -> VehicleService.addVehicle();
+                case NEXT_VEHICLE -> VehicleService.readNextVehicle();
+                case EXIT -> VehicleService.checkQueue();
+                default -> System.out.println("Spróbuj jeszcze raz");
+            }
+        } while (userInput != EXIT);
+    }
+
+    private static void printOptions() {
+        System.out.println(CREATE_AND_ADD_VEHICLE + " - Dodanie pojazdu");
+        System.out.println(NEXT_VEHICLE + " - Wczytaj następny pojazd");
+        System.out.println(EXIT + " - Wyjście z programu");
     }
 }
